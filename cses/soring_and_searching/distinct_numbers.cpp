43fce0909596c6a8840864af3fd3c76e30d56f85
spectra1 <sauat.bolat@icloud.com>
2026-07-11 17:42:01 +0500

    Solution for Distinct Numbers
    This program reads a number of integers and outputs the count of distinct numbers using a set.

---
+++ cses/soring_and_searching/distinct_numbers.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using vll = vector<ll>;
+using ull = unsigned long long;
+
+#define pb push_back
+#define sz(x) int(x.size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+const ll INF = 2e18;
+const int maxn = 1e7 + 10;
+const ll MOD = 1e9 + 7;
+const ll mod2 = 998244353;
+const ull pHash = 1000003;
+
+void solve() {
+    ll n;
+    cin >> n;
+    set<ll> st;
+    for(ll i = 0; i < n; ++i) {
+        ll x;
+        cin >> x;
+        st.insert(x);
+    }
+    cout << sz(st) << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // freopen("input.txt", "r", stdin);
+    //  freopen("output.txt", "w", stdout);
+    ll t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
