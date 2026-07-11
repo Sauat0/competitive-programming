b33fed3fd1ec78474e50f55a9bf777283fbcf26d
spectra1 <sauat.bolat@icloud.com>
2026-07-11 18:17:46 +0500

    Solution for Movie Festival
    Implement solution for the movie festival problem using sorting and greedy approach.

---
+++ cses/soring_and_searching/movie_festival.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using vll = vector<ll>;
+using ull = unsigned long long;
+
+#define pb push_back
+#define sz(x) int(x.size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+const ll INF = 2e18;
+const int maxn = 1e7 + 10;
+const ll MOD = 1e9 + 7;
+const ll mod2 = 998244353;
+const ull pHash = 1000003;
+
+void solve() {
+    ll n, cur = 0, ans = 0;
+    cin >> n;
+    vector<pair<ll, ll>> a(n);
+    for(auto& [x, y] : a) cin >> y >> x;
+    sort(all(a));
+    for(auto& [y, x] : a) {
+        if(x < cur) continue;
+        ans++;
+        cur = y;
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // freopen("input.txt", "r", stdin);
+    //  freopen("output.txt", "w", stdout);
+    ll t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
